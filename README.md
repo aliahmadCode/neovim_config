8b6f874a722fbd03cf6f8189fa63f95cf71d2b0a
Ali Ahmad <aliahmadgujjar895@gmail.com>
2025-01-30 19:15:23 +0500

    commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# The Vim Plugin, I Like.
--- lua/ali/lazy/markdown.lua
+++ lua/ali/lazy/markdown.lua
@@ -2,8 +2,9 @@ return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     build = "cd app && yarn install",
-    init = function()
-        vim.g.mkdp_filetypes = { "markdown" }
-    end,
-    ft = { "markdown" },
+  config = function()
+    vim.g.mkdp_auto_start = 0
+    vim.g.mkdp_browser = "chromium"
+  end,
+  ft = { "markdown" },
 }
