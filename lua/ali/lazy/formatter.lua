38bf38c655795b862ca5a3123730277ccf21ee08
Ali Ahmad <aliahmadgujjar895@gmail.com>
2025-02-01 14:17:18 +0500

    2

--- lua/ali/lazy/formatter.lua
+++ lua/ali/lazy/formatter.lua
@@ -1,12 +1,12 @@
 return {
-    'jose-elias-alvarez/null-ls.nvim',
-    config = function()
-        require('null-ls').setup({
-            sources = {
-                require('null-ls').builtins.formatting.prettier.with({
-                    extra_args = { "--tab-width", "4" }
-                })
-            },
+  'jose-elias-alvarez/null-ls.nvim',
+  config = function()
+    require('null-ls').setup({
+      sources = {
+        require('null-ls').builtins.formatting.prettier.with({
+          extra_args = { "--tab-width", "2" }
         })
-    end,
+      },
+    })
+  end,
 }
